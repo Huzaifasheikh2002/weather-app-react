9b87af14a1ee82b7262650350951bd2ed7a4ec0f
huzaifasheikh2002 <huzaifanadeem621@gmail.com>
2022-09-25 01:13:11 +0500

    ok

--- src/Componets/WeatherApp.jsx
+++ src/Componets/WeatherApp.jsx
@@ -1,84 +1,53 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-
-
-
 const WeatherApp = () => {
-
-    const apiKey =["e7d5e50aa5818b0de38b67d1599dc980"]
-    const [inputValue,SetinputValue]=useState("")
-    const [data,setData]=useState("")
-
-
-    const getWeatherDetails=(CityName)=>{
-        if(!CityName) return    
-const apiURL=`https://api.openweathermap.org/data/2.5/weather?q=${CityName}&appid=${apiKey}`
-axios.get(apiURL)
-.then((response)=>{
-// console.log("huziiii res",response);
-setData(response.data)
-}).catch((error)=>{
-
-
-    // console.log("error",error);
-  
-})
-}
-useEffect(()=>{
-getWeatherDetails("karachi")
-
-},[])
-
-
-const handleForm=(e)=>{
-    e.preventDefault();
-    if (!inputValue ) {
-        // console.log("enter some value");
-        return;
+    const apiKey = ["e7d5e50aa5818b0de38b67d1599dc980"]
+    const [inputValue, SetinputValue] = useState("")
+    const [data, setData] = useState("")
+    const getWeatherDetails = (CityName) => {
+        if (!CityName) return
+        const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${CityName}&appid=${apiKey}`
+        axios.get(apiURL)
+            .then((response) => {
+                // console.log("huziiii res",response);
+                setData(response.data)
+            }).catch((error) => {
+ // console.log("error",error);
+            }) }
+    useEffect(() => {
+        getWeatherDetails("karachi")
+    }, [])
+    const handleForm = (e) => {
+        e.preventDefault();
+        if (!inputValue) {
+            return;
+        } }
+    const handleSearch = () => {
+        getWeatherDetails(inputValue)
+        SetinputValue("")
+    }
+    const handleInput = (e) => {
+        SetinputValue(e.target.value)
       }
-}
-
-const handleSearch =()=>{
-    
-getWeatherDetails(inputValue)
-SetinputValue("")
-}
-
-
-const handleInput=(e)=>{
-
-    SetinputValue(e.target.value)
-}
-  return (
-    <div className='Main'>
-        <div className='Bg'>
-
-            <h1 className='heading'>Weather App</h1>
-            
-            <form onSubmit={handleForm} className=' d-grid gap-3 col-4 mt-4 '>
-        
-            <input  onChange={handleInput}  
-            value={inputValue}
-            placeholder="Enter Country,City Name..."
-             type="text" className='form-control '/>
-             
-            <button onClick={handleSearch} className='btn btn-primary mt-4 SearchBtn'>Search</button>
-            </form>
-            
-        </div>
-<section className='col-md-12 text-align center mt-10'>
-<div className='weatherResultBox'>
-<img className="weathorIcon"
-              src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" />
-<h5 className='WeatherCity'>{data?.name}</h5>
-<h6 className='WeatherTemp'>{((data?.main?.temp)-273.15).toFixed(2)}°C</h6>
-</div>
-
-
-</section>
-
-    </div>
-)
-}
-
-export default WeatherApp
+    return (
+        <div className='Main'>
+            <div className='Bg'>
+                <nav className="navbar navbar" >
+                    <div className="container-fluid col-10">
+                        <h1 className='heading'>Weather App</h1>
+                        <form onSubmit={handleForm} className="d-flex">
+                            <input onChange={handleInput} className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button onClick={handleSearch} className="btn btn-primary  " type="submit">Search</button>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+            <section className=''>
+                <div className='weatherResultBox'>
+                    <img className="weathorIcon"
+                        src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" />
+                    <h5 className='WeatherCity'>{data?.name}</h5>
+                    <h6 className='WeatherTemp'>{((data?.main?.temp) - 273.15).toFixed(2)}°C</h6>
+                </div>
+            </section> 
+            </div> )} ;export default WeatherApp
